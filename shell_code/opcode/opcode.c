1b1ba3104072598772f45f221bf3855168abc01c
Tyler Lukasiewicz <tlukasiewicz@knights.ucf.edu>
2016-01-21 20:28:23 -0500

    made opcode cooler

--- shell_code/opcode/opcode.c
+++ shell_code/opcode/opcode.c
@@ -5,10 +5,12 @@ int x;
 void func()
 {
     char buf[10];
+    puts("enter your name: ");
     scanf("%s", buf);
 }
 int main()
 {
+    puts("enter your age: ");
     scanf("%d",&x);
     func();
     return 0;
