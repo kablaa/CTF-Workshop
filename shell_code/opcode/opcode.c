87141f7b90846bbd77bc6afd1ac8b9f452ba02f7
Tyler Lukasiewicz <tlukasiewicz@knights.ucf.edu>
2016-01-21 19:54:37 -0500

    make opcode cooler

--- shell_code/opcode/opcode.c
+++ shell_code/opcode/opcode.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <stdio.h>
 
-char x[4];
+int x;
 void func()
 {
     char buf[10];
-    scanf("%s",x);
     scanf("%s", buf);
 }
 int main()
 {
+    scanf("%d",&x);
     func();
     return 0;
 }
-
