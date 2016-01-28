0d3e8fba404e24efcceb7c5d93d3526f14db0f5d
Tyler Lukasiewicz <tlukasiewicz@knights.ucf.edu>
2016-01-28 17:36:55 -0500

    fixed c file

--- heap/heap3/heap3.c
+++ heap/heap3/heap3.c
@@ -10,13 +10,13 @@ int main(int argc, char *argv[])
                 printf("Usage Error\n");
                 return;
         }
-        [1]buf1 = malloc(256);
-        [2]strcpy(buf1, argv[1]); /* Prereq 1 */
-        [3]buf2 = malloc(strtoul(argv[2], NULL, 16)); /* Prereq 2 */
-        [4]buf3 = malloc(256); /* Prereq 3 */
-        [5]strcpy(buf3, argv[3]); /* Prereq 3 */
+        buf1 = malloc(256);
+        strcpy(buf1, argv[1]);
+        buf2 = malloc(strtoul(argv[2], NULL, 16));
+        buf3 = malloc(256);
+        strcpy(buf3, argv[3]);
 
-        [6]free(buf3);
+        free(buf3);
         free(buf2);
         free(buf1);
         return 0;
