87b2871417eef80d980d8609021244ee1e87046a
kablaa <tlukasiewicz@knights.ucf.edu>
2016-02-15 09:17:19 -0500

    made stuff nicer

--- writeups/ropasaurusrex/Rop.py
+++ writeups/ropasaurusrex/Rop.py
@@ -4,32 +4,26 @@ class RopChain:
 
     __doc__=\
     '''
-########################################################################################
 
     RopChain is a class that was designed to make life easier while creating ROP exploits.
 
-    You can initilize your ropchain with
-
-     ropChain = RopChain()
 
     There are two ways to create a rop chain:
 
-        1) Add the pop_pop_ret addresses before creating the chain. This is done with the addPopRet() function, which takes two arguments. The first is the number of pops, the second is the addres. So for example if I want to call a function that takes three parameters, I would do
+        1) Add the pop_pop_ret addresses before creating the chain.
 
-        ropChain.addPopRet(3,cleanup_addr)
-        ropChain.addLink(func_addr,[p1,p2,p3])
+            ropChain.addPopRet(3,cleanup_addr)
+            ropChain.addLink(func_addr,[p1,p2,p3])
 
-        where cleanup_addr is the address of a pop_pop_pop_ret, func_addr is the address of the fuction, and p1,p2, and p3 are the parameters
 
         2) explicitaly add the address of each cleanup for each link. For exlample
 
-        ropCHain.addLink(func_addr, [p1,p2,..,pn], pop*n_r)
+            ropCHain.addLink(func_addr, [p1,p2,..,pn], pop*n_r)
 
     When you are ready to create your payload, you can get the full rop chain with
 
     ropChain.getChain()
 
-########################################################################################
     '''
 
 
